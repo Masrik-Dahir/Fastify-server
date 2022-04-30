02b104309ba240704a63ed7bdaabec0a4b513a55
masri <dahirma@vcu.edu>
2022-04-30 17:53:37 -0400

    all methods done

--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,4 @@
-const {getItems, getItem, addItem} = require('../controllers/items')
+const {getItems, getItem, addItem, deleteItem, updateItem} = require('../controllers/items')
 
 // Item Schema
 const Item = {
@@ -27,14 +27,14 @@ const getItemsOpts = {
 const getItemOpts = {
     schema: {
         response : {
-            201: Item
+            200: Item
         },
     },
     handler: getItem,
 }
 
-// Option for get all items
-const postItemOps = {
+// Option for adding items
+const postItemOpts = {
     schema: {
         body:{
             type: 'object',
@@ -46,11 +46,39 @@ const postItemOps = {
             }
         },
         response : {
-            200: Item
+            201: Item
         },
     },
     handler: addItem,
 }
+
+// Option for deleting items
+const deleteItemOpts = {
+    schema: {
+        response : {
+            200: {
+                type: 'object',
+                properties: {
+                    message: {
+                        type: 'string'
+                    }
+                }
+            }
+        },
+    },
+    handler: deleteItem,
+}
+
+// Option for get all items
+const updateItemOpts = {
+    schema: {
+        response : {
+            200: Item
+        },
+    },
+    handler: updateItem,
+}
+
 
 function itemRoutes  (fastify, options, done) {
     // Get all items
@@ -60,7 +88,13 @@ function itemRoutes  (fastify, options, done) {
     fastify.get('/items/:id', getItemOpts)
 
     // Add item
-    fastify.post('/items', postItemOps)
+    fastify.post('/items', postItemOpts)
+
+    //Delete items
+    fastify.delete('/items/:id', deleteItemOpts)
+
+    //Update items
+    fastify.put('/items/:id', updateItemOpts)
 
     
     done()
