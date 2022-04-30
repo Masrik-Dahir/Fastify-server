c404a0c7b6c7d05f7ee0907cd2c6e4f7a6d78e47
masri <dahirma@vcu.edu>
2022-04-30 18:32:16 -0400

    datga added

--- items.js
+++ items.js
@@ -1,7 +1,7 @@
 let items = [
     {
         id: '1', 
-        name: 'Item One', 
+        name: 'First Employee', 
         email: 'abc@capitalone.com', 
         phone: '8042346835', 
         hire_date: '2021-01-01',
@@ -12,10 +12,54 @@ let items = [
         department_id: '10'
     },
 
-    {id: '2', name: 'Item Two'},
-    {id: '3', name: 'Item Three'},
-    {id: '4', name: 'Item Four'},
-    {id: '5', name: 'Item Five'}
+    {
+        id: '2', 
+        name: 'Second Employee',
+        email: 'dfg@capitalone.com', 
+        phone: '1235932479', 
+        hire_date: '2014-01-01',
+        job_id: '24',
+        salary: 80000,
+        commission_pct: 0,
+        manager_id: null,
+        department_id: '10'
+    },
+    {
+        id: '3', 
+        name: 'Third Employee',
+        email: 'ret@capitalone.com', 
+        phone: '2345634590', 
+        hire_date: '2022-01-23',
+        job_id: '30',
+        salary: 200000,
+        commission_pct: 1,
+        manager_id: null,
+        department_id: '20'
+    },
+    {
+        id: '4', 
+        name: 'Fourth Employee',
+        email: 'fdsf@capitalone.com', 
+        phone: '8042372439', 
+        hire_date: '2008-01-23',
+        job_id: '21',
+        salary: 250000,
+        commission_pct: 5,
+        manager_id: null,
+        department_id: '10'
+    },
+    {
+        id: '5', 
+        name: 'Fifth Employee', 
+        email: 'abc@capitalone.com', 
+        phone: '8042346835', 
+        hire_date: '2011-01-01',
+        job_id: '24',
+        salary: 50000,
+        commission_pct: 0,
+        manager_id: '4',
+        department_id: '10'
+    }
 ]
 
 module.exports = items
