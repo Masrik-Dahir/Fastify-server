d36817c3a15aa1a4de24676bb57f3c1b61edd700
masri <dahirma@vcu.edu>
2022-04-30 16:17:50 -0400

    initial

---
+++ requirements.sh
@@ -0,0 +1,3 @@
+npm init -y
+npm install fastify fastify-swagger uuid
+npm i -D nodemon
---
+++ routes/items.js
@@ -0,0 +1,50 @@
+
+const items = require ('../Items')
+
+// Option for get all items
+const getItemsOpts = {
+    schema: {
+        response : {
+            200: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        id: {type: 'integer'},
+                        name: {type: 'string'}
+                    }
+                }
+            }
+        }
+    }
+}
+
+const getItemOpts = {
+    schema: {
+        response : {
+            200: {
+                type: 'object',
+                    properties: {
+                        id: {type: 'integer'},
+                        name: {type: 'string'}
+                    } 
+            }
+        }
+    }
+}
+
+function itemRoutes  (fastify, options, done) {
+    // Get all items
+    fastify.get('/items', getItemsOpts, (req, reply) => {
+        reply.send(items)
+    })
+    
+    // Get single items
+    fastify.get('/items/:id', getItemOpts, (req, reply) => {
+        const {id} = req.params
+        const item = items.find(item => item.id === id)
+        reply.send(item)
+    })
+    done()
+}
+module.exports = itemRoutes
